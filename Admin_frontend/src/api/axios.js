f8225fefc61dcfa8e963f4c55b795e19a6ea7fe5
anvit <singhalanvit534@gmail.com>
2026-01-13 00:47:37 +0530

    url fix 2.0:)

--- Admin_frontend/src/api/axios.js
+++ Admin_frontend/src/api/axios.js
@@ -4,7 +4,7 @@ import axios from "axios";
 // If we are on Vercel, it uses the hidden environment variable.
 // If we are on Localhost, it uses port 8000.
 // We append "/api" because your backend routes start with /api
-const BASE_URL = (import.meta.env.VITE_API_URL ||"http://localhost:8000") + "/api";
+const BASE_URL = (import.meta.env.VITE_API_URL || "https://divyaveda.onrender.com" || "http://localhost:8000") + "/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
@@ -26,7 +26,7 @@ api.interceptors.response.use(
   res => res,
   err => {
     const status = err.response?.status;
-    
+
     // 401: Unauthorized -> Force Logout
     if (status === 401) {
       localStorage.removeItem("adminToken");
